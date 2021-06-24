d5537b4fd4a93cbbddc1b639980d8ebc3700c2da
Lerato M <79574031+Lerato029@users.noreply.github.com>
2021-06-24 21:35:55 +0200

    google analytics config

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,28 @@ import "../styles/globals.css";
 /* import layout style for uniformity across all pages */
 import Layout from "../components/Layout";
 
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import * as ga from "../lib/g-analytics";
 //parent component that is also wrapped in the Layout component
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <Layout>
       <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ export default function Home() {
     <div>
       <head>
         <title>Home</title>
+        
       </head>
       <div className="profilePic">
         <Image
